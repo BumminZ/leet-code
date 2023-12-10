e87187b5edcbb24a0bc2d1022711c3329e826d79
Aung Tay Zar Maung <cryptoice2001@gmail.com>
2023-12-11 05:54:48 +0630

    updated tm_867.rs

--- tm_867.rs
+++ tm_867.rs
@@ -1,15 +1,14 @@
 impl Solution {
     pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let mut result = Vec::new();
+        // This creates a vector filled with zeros (0) and its length is equal to the length of the first row of the matrix (assuming all rows have the same length).
+        // The matrix.len() as usize is used to convert the length to the usize type.
+        let mut resp: Vec<Vec<i32>> = vec![vec![0; matrix.len() as usize]; matrix[0].len()];
 
-        for i in 0..matrix[0].len() {
-            let mut col = Vec::new();
-            for j in 0..matrix.len() {
-                col.push(matrix[j][i]);
+        for i in 0..resp.len() {
+            for j in 0..resp[0].len() {
+                resp[i][j] = matrix[j][i];
             }
-            result.push(col)
         }
-
-        return result;
+        return resp;
     }
 }
